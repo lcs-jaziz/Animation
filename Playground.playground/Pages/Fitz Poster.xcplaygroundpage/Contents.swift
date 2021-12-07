b950b52e65fc5f2a977ee8f2d26e86ea4f99cb68
Jimena Aziz <jaziz@lcs.on.ca>
2021-12-07 13:50:17 -0500

    Fitz Poster gradient background from light grey to dark grey

--- Playground.playground/Pages/Fitz Poster.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Fitz Poster.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
  
  Set the size of your desired canvas by adjusting the constants on lines 7 and 8.
  */
-let preferredWidth = 600
+let preferredWidth = 400
 let preferredHeight = 600
 /*:
  ## Required code
@@ -41,11 +41,11 @@ PlaygroundPage.current.liveView = canvas
  */
 
 // Move the origin from the bottom-left corner of the canvas to it's centre point
-canvas.translate(to: Point(x: canvas.width / 2,
-                           y: canvas.height / 2))
+//canvas.translate(to: Point(x: canvas.width / 2,
+                           //y: canvas.height / 2))
 
 // Show a grid
-canvas.drawAxes(withScale: true, by: 20, color: .black)
+canvas.drawAxes(withScale: true, by: 50, color: .black)
 
 /*:
  ## Add your code
@@ -57,21 +57,20 @@ canvas.drawAxes(withScale: true, by: 20, color: .black)
  */
 
 // Begin writing your code below (you can remove the examples shown)
+canvas.highPerformance = true
+for someValue in stride(from: 1.0, through: 600.0, by: 1.0) {
+    
+    someValue
+    let currentBrightness = map(value: someValue, fromLower: 1.0, fromUpper: 600.0, toLower: 35.27, toUpper: 82.75)
+    
+    canvas.lineColor = Color(hue: 0.0, saturation: 0.0, brightness: Float(currentBrightness), alpha: 100.0)
+    canvas.drawLine(from: Point(x: 0, y: someValue),
+                    to: Point(x: 400, y: someValue))
 
-// Draw a circle, using the canvas object directly
-canvas.drawEllipse(at: Point(x: 100, y: 100), width: 25, height: 25)
+    
+}
+canvas.highPerformance = false
 
-// Draw a vertical line, up and to the left
-p.drawTo(dx: -25, dy: 50)
-
-// Go back to origin
-p.goToOrigin()
-
-// Change the pen color
-p.penColor = .red
-
-// Draw a curve, down and to the right
-p.addArc(radius: 50, angle: -45)
 
 /*:
  ## Show the Live View
